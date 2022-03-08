b2fbd11482b87b5d82252c079aa538ce5cd69605
Frus <mcagdasbayirli@gmail.com>
2022-03-08 18:07:16 +0300

    seo fixes

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,9 +9,14 @@ export default {
     site_name: "Deriate",
     images: [
       {
-        url: "https://media.graphcms.com/hRN5yfVQTKbRcZk8UUN7?_ga=2.158163578.1788949347.1646580573-1778747608.1642625803",
+        url: "https://media.graphcms.com/udSiNz6RgOFfqJ29yGVQ?_ga=2.180357607.1788949347.1646580573-1778747608.1642625803",
         alt: "Deriate",
       },
     ],
+  },
+  twitter: {
+    handle: "@deriateblog",
+    site: "@deriateblog",
+    cardType: "summary_large_image",
   },
 };
